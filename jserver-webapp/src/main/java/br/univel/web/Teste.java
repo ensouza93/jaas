6ad1d99f9c6dd4545403b00c90454bb5b60625de
Fernando D'Agostini <fdagostini@gmail.com>
2015-12-03 21:20:08 -0200

    Aplicação web segura.

---
+++ jserver-webapp/src/main/java/br/univel/web/Teste.java
@@ -0,0 +1,33 @@
+package br.univel.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = "teste")
+public class Teste extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+}
